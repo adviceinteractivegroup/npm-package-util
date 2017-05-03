2d559fd22a0edbb15c0bdc2de3f8001a00c15d86
Maleja <m.aleja3000@gmail.com>
2017-05-03 11:16:42 -0500

    Update RemoveSomeCharacters test

--- test/removeSomeCharactersç.test.js
+++ test/removeSomeCharactersç.test.js
@@ -2,31 +2,21 @@
 const assert = require('chai').assert;
 const removeSomeCharacters = require('../lib/removeSomeCharacters');
 
-describe('Remove Some Characters for Plus', function removeSomeCharactersDescribe() {
-  let text = 'CLAFOUTIS%x26CHARLOTA';
-  it('removeSomeCharacter should return a text A clean text, without spaces of more, nor characters not allowed', function dataProcessingIt() {
-    let result = removeSomeCharacters.removeSomeCharactersForPlus(text);
-    assert.isString(result);
-    assert.equal(result, 'CLAFOUTIS+x+CHARLOTA');
-  });
-
-  it('removeSomeCharacter should NOT return a text A clean text', function removeSomeCharacterErrIt() {
-    let result = removeSomeCharacters.removeSomeCharactersForPlus(text);
-    assert.isString(result);
-    assert.equal(result.indexOf('%'), -1);
-  });
-});
-
-describe('Remove Some Characters for Empty', function removeSomeCharactersDescribe() {
+describe('Remove Some Characters', function removeSomeCharactersDescribe() {
   let text = 'Yellow%x26+Red Blue';
-  it('removeSomeCharacter should return a text A clean text, without spaces of more, nor characters not allowed', function dataProcessingIt() {
-    let result = removeSomeCharacters.removeSomeCharactersForEmpty(text);
+  it('removeSomeCharacters should return a text A clean text, without spaces of more, nor characters not allowed', function dataProcessingIt() {
+    let result = removeSomeCharacters.removeSomeCharacters(text, '+');
     assert.isString(result);
-    assert.equal(result, 'Yellow x  Red Blue');
+    assert.equal(result, 'Yellow+x++Red+Blue');
+  });
+    it('removeSomeCharacters should return a text A clean text, without spaces of more, nor characters not allowed', function dataProcessingIt() {
+    let result = removeSomeCharacters.removeSomeCharacters(text, ' ');
+    assert.isString(result);
+    assert.equal(result, 'Yellow x +Red Blue');
   });
 
   it('removeSomeCharacter should NOT return a text A clean text', function removeSomeCharacterErrIt() {
-    let result = removeSomeCharacters.removeSomeCharactersForEmpty(text);
+    let result = removeSomeCharacters.removeSomeCharacters(text);
     assert.isString(result);
     assert.equal(result.indexOf('%'), -1);
   });
