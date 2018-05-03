27b88ef569e46afc020d4b193a52146b49b44c3e
pieroadvice <piero@adviceinteractive.com>
2018-05-03 18:16:56 -0500

    sorensen function changed

--- test/sorensen.test.js
+++ test/sorensen.test.js
@@ -3,19 +3,19 @@ const assert = require('chai').assert;
 const sorensen = require('../lib/sorensen');
 
 describe('sorensen', function () {
-  it("sorensen should return 1 when comparing 'xxxxxxxxxxxxxxxxxxxx' to 'txxxxtttttxxxxxxxttt'", function () {
-    assert.equal(sorensen('xxxxxxxxxxxxxxxxxxxx', 'txxxxtttttxxxxxxxttt'), 1); //disagree with this result
+  it("sorensen should return 0.47368421052631576 when comparing 'xxxxxxxxxxxxxxxxxxxx' to 'txxxxtttttxxxxxxxttt'", function () {
+    assert.equal(sorensen('xxxxxxxxxxxxxxxxxxxx', 'txxxxtttttxxxxxxxttt'), 0.47368421052631576); //disagree with this result
   });
   
-  it("sorensen should return 0.9 when comparing 'Advice interactive group' and 'advice interactive'", function () {
-    assert.equal(sorensen('Advice interactive group', 'advice interactive'), 0.9);
+  it("sorensen should return 0.8 when comparing 'Advice interactive group' and 'advice interactive'", function () {
+    assert.equal(sorensen('Advice interactive group', 'advice interactive'), 0.8);
   });
   
-  it("sorensen should return 0.7058823529411765 when comparing 'Advice interactive group' and 'advice inter'", function () {
-    assert.equal(sorensen('Advice interactive group', 'advice inter'), 0.7058823529411765);
+  it("sorensen should return 0.5882352941176471 when comparing 'Advice interactive group' and 'advice inter'", function () {
+    assert.equal(sorensen('Advice interactive group', 'advice inter'), 0.5882352941176471);
   });
   
   it("sorensen should return 0.7058823529411765 when comparing 'Advice interactive group' and 'McDonald's'", function () {
-    assert.equal(sorensen('Advice interactive group', "McDonald's"), 0);
+    assert.equal(sorensen('McDonald\'s', 'Advice interactive group'), 0);
   });
 });
