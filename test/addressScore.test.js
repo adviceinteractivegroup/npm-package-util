d50dbda08319f10a64207acb82aa0b4f00eb6c2d
pieroadvice <piero@adviceinteractive.com>
2019-06-24 17:22:50 -0500

    fixed issue (scoring incorrectly postalcode) with the address scoring with hide addr=true

--- test/addressScore.test.js
+++ test/addressScore.test.js
@@ -9,14 +9,14 @@ describe('getAddressScore', () => {
       street_address1: '7850 Collin Mckinney pkwy',
       city: 'Mckinney',
       state: 'TX',
-      postalCode: '75070',
+      postal_code: '75070',
       country: null
     };
     let add2 = { 
       street_address1: '7850 Collin Mckinney pkwy',
       city: 'Mckinney',
       state: 'TX',
-      postalCode: '75070',
+      postal_code: '75070',
       country: null
     };
     let result = getAddressScore(add1, add2);
@@ -28,14 +28,14 @@ describe('getAddressScore', () => {
       street_address1: '',
       city: 'Mckinney',
       state: 'Texas',
-      postalCode: '75070',
+      postal_code: '75070',
       country: null 
     };
     let add2 = { 
       street_address1: '14747 N  Blvd',
       city: 'Scottsdale',
       state: 'AZ',
-      postalCode: '85260',
+      postal_code: '85260',
       country: null
     };
     let result = getAddressScore(add1, add2);
@@ -47,7 +47,7 @@ describe('getAddressScore', () => {
       street_address1: '7850 Collin Mckinney pkwy',
       city: 'Scottsdale',
       state: 'AZ',
-      postalCode: '85260',
+      postal_code: '85260',
       country: null,
       hide: true
     };
@@ -55,7 +55,7 @@ describe('getAddressScore', () => {
       street_address1: '',
       city: 'Scottsdale',
       state: 'AZ',
-      postalCode: '85260',
+      postal_code: '85260',
       country: null
     };
     let result = getAddressScore(add1, add2);
@@ -72,7 +72,7 @@ describe('getAddressScore', () => {
       street_address1: '7850 Collin Mckinney pkwy',
       city: 'Scottsdale',
       state: 'AZ',
-      postalCode: '85260',
+      postal_code: '85260',
       country: null,
       hide: true
     };
@@ -80,32 +80,57 @@ describe('getAddressScore', () => {
       street_address1: null,
       city: 'Scottsdale',
       state: 'AZ',
-      postalCode: '85260',
+      postal_code: '85260',
       country: null
     };
     let result = getAddressScore(add1, add2);
     should.exist(result);
     expect(result).to.equal(1);
   });
-  it('should return score = 0.6~ because hide addr = true & all addr segments to score are = true, but addr2.state != addr1.state', ()=>{
-    let add1 = { 
-      hideRules : {
-        postalCode : true,
+  it('should return score = 0.6~ because hide addr = true & all addr segments to score are = true, but addr2.state != addr1.state', () => {
+    let add1 = {
+      hideRules: {
+        postalCode: true,
         city: true,
         state: true
       },
       street_address1: '7850 Collin Mckinney pkwy',
       city: 'Scottsdale',
       state: 'AZ',
-      postalCode: '85260',
+      postal_code: '85260',
       country: null,
       hide: true
     };
-    let add2 = { 
+    let add2 = {
       street_address1: null,
       city: 'Scottsdale',
       state: null,
-      postalCode: '85260',
+      postal_code: '85260',
+      country: null
+    };
+    let result = getAddressScore(add1, add2);
+    should.exist(result);
+    expect(result).to.equal(0.6666666666666666);
+  });
+  it('should return score = 0.6~ because hide addr = true & all addr segments to score are = true, but addr2.postal_code != addr1.postal_code', () => {
+    let add1 = {
+      hideRules: {
+        postalCode: true,
+        city: true,
+        state: true
+      },
+      street_address1: '7850 Collin Mckinney pkwy',
+      city: 'Scottsdale',
+      state: 'AZ',
+      postal_code: '85261',
+      country: null,
+      hide: true
+    };
+    let add2 = {
+      street_address1: null,
+      city: 'Scottsdale',
+      state: 'AZ',
+      postal_code: '85260',
       country: null
     };
     let result = getAddressScore(add1, add2);
@@ -122,7 +147,7 @@ describe('getAddressScore', () => {
       street_address1: null,
       city: 'Scottsdale',
       state: 'AZ',
-      postalCode: '85260',
+      postal_code: '85260',
       country: null,
       hide: true
     };
@@ -130,7 +155,7 @@ describe('getAddressScore', () => {
       street_address1: '7850 Collin Mckinney pkwy',
       city: 'Scottsdale',
       state: 'AZ',
-      postalCode: '85260',
+      postal_code: '85260',
       country: null
     };
     let result = getAddressScore(add1, add2);
